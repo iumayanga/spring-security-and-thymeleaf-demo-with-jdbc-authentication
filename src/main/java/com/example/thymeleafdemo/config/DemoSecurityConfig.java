e38d37c54448ae0dd10faff34fc71ec702ace7c8
iumayanga <pl.isuru97@gmail.com>
2022-10-04 22:27:44 +0530

    Remove deprecated WebSecurityConfigurerAdapter

--- src/main/java/com/example/thymeleafdemo/config/DemoSecurityConfig.java
+++ src/main/java/com/example/thymeleafdemo/config/DemoSecurityConfig.java
@@ -2,59 +2,56 @@ package com.example.thymeleafdemo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
-public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
-
-	// add a reference to our security data source
+public class DemoSecurityConfig {
 
 	@Autowired
 	@Qualifier("securityDataSource")
 	private DataSource securityDataSource;
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-		// use jdbc authentication
-		auth.jdbcAuthentication().dataSource(securityDataSource);
-
+	@Bean
+	public UserDetailsManager userDetailsManager() {
+		return new JdbcUserDetailsManager(securityDataSource);
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-		http.authorizeRequests()
-			.antMatchers("/employees/showForm*").hasAnyRole("MANAGER", "ADMIN")
-			.antMatchers("/employees/save*").hasAnyRole("MANAGER", "ADMIN")
-			.antMatchers("/employees/delete").hasRole("ADMIN")
-			.antMatchers("/employees/**").hasRole("EMPLOYEE")
-//			.antMatchers("/resources/**").permitAll()
-			.and()
-			.formLogin()
-				.loginPage("/showMyLoginPage")
-				.loginProcessingUrl("/authenticateTheUser")
-				.permitAll()
-			.and()
-			.logout().permitAll()
-			.and()
-			.exceptionHandling().accessDeniedPage("/access-denied");
-		
+		return http
+				.authorizeRequests(configurer ->
+						configurer
+//							.anyRequest().authenticated()
+							.antMatchers("/employees/showForm*").hasAnyRole("MANAGER", "ADMIN")
+							.antMatchers("/employees/save*").hasAnyRole("MANAGER", "ADMIN")
+							.antMatchers("/employees/delete").hasRole("ADMIN")
+							.antMatchers("/employees/**").hasRole("EMPLOYEE")
+							.antMatchers("/resources/**").permitAll())
+
+				.formLogin(configurer ->
+						configurer
+								.loginPage("/showMyLoginPage")
+								.loginProcessingUrl("/authenticateTheUser")
+								.permitAll())
+
+				.logout(configurer ->
+						configurer
+								.permitAll())
+
+				.exceptionHandling(configurer ->
+						configurer
+								.accessDeniedPage("/access-denied"))
+
+				.build();
 	}
-		
 }
-
-
-
-
-
-
